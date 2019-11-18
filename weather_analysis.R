c03092a5f05ffe5bd41f13a6580e4623da40b12e
jamesswartwood <jamesms@cs.washington.edu>
2019-11-18 14:12:02 -0800

    Replaced the weather data frame and updated the analysis file

--- weather_analysis.R
+++ weather_analysis.R
@@ -1,10 +1,4 @@
-weather_df <-
-  read.csv("/Users/jayrg007/Desktop/INFO201/data-science-project-info-201/data/weather.csv",
-           stringsAsFactors = FALSE)
-
-show_by_given_temp <- function(temperature) {
-  a <- weather_df %>%
-    select(temp == temperature)
-}
+weather_df <- read.csv("data/weather.csv", stringsAsFactors = FALSE)
+CO2_df <- read.csv("data/CO2_data.csv", stringsAsFactors = FALSE)
 
 
